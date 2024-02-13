ea29d0043dcb7a5567d64c5388f4deb6f457cbf6
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-02-13 14:47:01 +0000

    [ADDED] /users/:path_username

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+
+    @list_of_users = matching_users.order({ :username => :asc})
+    render({:template => "user_templates/index"})
+  end
+
+  def show
+    the_username = params.fetch("path_username")
+    @the_user = User.where({:username => the_username}).first
+    
+    render({:template => "user_templates/show"})
+  end
+
+  def create
+    @the_user = User.new
+    @the_user.username = params.fetch("input_username")
+
+    if @the_user.username.present?
+      @the_user.save
+      redirect_to("/users/", { :notice => "User created successfully."} )
+    else
+      redirect_to("/users", { :alert => "User failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    matching_user = User.find(the_id)
+    matching_user.username = params.fetch("input_username")
+
+    if matching_user.username.present?
+      matching_user.save
+      redirect_to("/users/#{matching_user.username}", { :notice => "User updated successfully."} )
+    else
+      redirect_to("/users", { :alert => "User failed to update successfully." })
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # ==== USER ====
+  get("/", { :controller => "users", :action => "index"})
+  get("/users", { :controller => "users", :action => "index"})
+  # create
+  post("/add_user", {:controller => "users", :action => "create"})
+  # show
+  get("/users/:path_username",{:controller => "users", :action => "show"})
+  # update
+  post("/update_user/:path_id", {:controller => "users", :action => "update"})
 
 end
